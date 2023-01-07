e6f1dce5227cd551bec76a941abb6a44ea92d0a4
khaled-aldasouki <kaa5082@g.rit.edu>
2023-01-07 18:20:34 +0400

    Improved Responsive Design

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,15 +6,22 @@ function scrollFunction() {
     var title = document.getElementById("title");
     var nav = document.getElementById("nav");
     var navButtons = document.getElementsByClassName("nav-button")
+    var menu = document.getElementById("menu");
 
   if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
     if (window.innerWidth > 768){
-      header.style.height = "10vh";
+      header.style.height = "7vh";
       nav.style.marginTop = "3vh";
-      title.style.fontSize = "2rem";
+      title.style.fontSize = "2.2rem";
+      menu.style.height = "4vh"
+      menu.style.width = "4vh"
+      title.style.margin = "auto 2vw auto 0";
+
     } 
     else {
-      header.style.height = "9vh"
+      header.style.height = "6vh"
+      
+      
     }
 
     nav.style.marginBottom = "8vh";
@@ -24,14 +31,17 @@ function scrollFunction() {
       } 
     else {
         if (window.innerWidth > 768){
-          header.style.height = "12vh";
-          title.style.margin = "2vh 3vw";
+          header.style.height = "8vh";
+          title.style.margin = "auto 2vw auto 0vw";
+          title.style.marginLeft = "0";
           nav.style.marginTop = "4vh";
           title.style.fontSize = "2.5rem";
+          menu.style.height = "5vh"
+          menu.style.width = "5vh"
+          
         } 
         else {
-          header.style.height = "9vh"
-          title.style.margin = "2vh 2vw";
+          header.style.height = "6vh"
         }
     
     nav.style.marginBottom = "10vh";
