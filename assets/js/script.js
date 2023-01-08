b3b562d5bb285ee5c5521ee75333408081efc3ce
khaled-aldasouki <kaa5082@g.rit.edu>
2023-01-08 04:54:59 +0400

    updated nav and responsiveness

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,10 @@
 window.onscroll = function() {scrollFunction()};
-
+window.onload = function(){menu()};
+window.onload(function() {
+  if (window.innerWidth >= 1600){
+    $("<p>hi</p>").appendTo( $("#header"));
+  }
+});
 function scrollFunction() {
   //make the navigation bar smaller when the user scrolls down 50 pixels
     var header = document.getElementById("header");
@@ -9,45 +14,44 @@ function scrollFunction() {
     var menu = document.getElementById("menu");
 
   if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    //scrolled on tablet
     if (window.innerWidth > 768){
       header.style.height = "7vh";
-      nav.style.marginTop = "3vh";
+      nav.style.marginTop = "7vh"
       title.style.fontSize = "2.2rem";
       menu.style.height = "4vh"
       menu.style.width = "4vh"
       title.style.margin = "auto 2vw auto 0";
-
     } 
-    else {
-      header.style.height = "6vh"
-      
-      
-    }
+    
 
-    nav.style.marginBottom = "8vh";
+    //scrolled on any device
+    /*
     for (button of navButtons){
         button.style.margin = "0 0.5vw";
-    }
-      } 
+    }*/
+      }
+
+    //didn't scoll on tablet 
     else {
         if (window.innerWidth > 768){
           header.style.height = "8vh";
+          nav.style.marginTop = "8vh"
           title.style.margin = "auto 2vw auto 0vw";
           title.style.marginLeft = "0";
-          nav.style.marginTop = "4vh";
           title.style.fontSize = "2.5rem";
           menu.style.height = "5vh"
           menu.style.width = "5vh"
           
         } 
-        else {
-          header.style.height = "6vh"
-        }
+
+        
+    //didn't scroll on any device
     
-    nav.style.marginBottom = "10vh";
+    /*
     for (button of navButtons){
         button.style.margin = "0 1vw";
-    }  }
+    } */ }
 }
 
 function pause(){
@@ -55,3 +59,18 @@ function pause(){
   if (vid.paused){vid.play()}
   else{vid.pause()}
 }
+
+//burger menu behavior
+function menu(){
+  var nav = document.getElementById('nav');
+  var navButtons = document.getElementsByClassName("nav-button")
+  if (nav.style.visibility == "hidden"){
+    nav.style.visibility = "visible";
+    nav.style.opacity = 1;
+  }
+  else{
+    if (window.innerWidth < 1600){nav.style.visibility = "hidden";nav.style.opacity = 0;
+  }
+  }
+}
+
